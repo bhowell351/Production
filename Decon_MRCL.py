0b111745238553850246043b56c9567d1f8de4aa
Ben Howell <benjamin.howell@intel.com>
2023-09-11 15:49:42 -0700

    Added Not Exist Table, Erases Input table

--- Decon_MRCL.py
+++ Decon_MRCL.py
@@ -1,4 +1,4 @@
-
+ 
 import numpy as np
 import time, PyUber, os, csv
 import warnings
@@ -6,7 +6,7 @@ from datetime import datetime
 import pandas as pd
 
 
-new_row = pd.read_csv(r"C:\Users\bhowell\OneDrive - Intel Corporation\ME\STRS upload test.csv")
+ent_input = pd.read_csv(r"C:\Users\bhowell\OneDrive - Intel Corporation\ME\STRS upload test.csv")
 warnings.simplefilter("ignore")
 
 atlas_sql = f"""
@@ -60,10 +60,10 @@ FROM D1D_PROD_XEUSS.P_F_UE_CONFIG P_F_UE_CONFIG
 
 
 
-for i, row in new_row.iterrows():
-    date = new_row.loc[i, 'DATE']
+for i, row in ent_input.iterrows():
+    date = ent_input.loc[i, 'DATE']
     new_date = date[0:10]+ " " +date[11:19]
-    new_row.loc[i,'DATE'] = new_date
+    ent_input.loc[i,'DATE'] = new_date
 
 
 conn_XEUS = PyUber.connect(datasource=('D1D_PROD_XEUS'))
@@ -71,35 +71,46 @@ atlas_frame = pd.read_sql(atlas_sql, conn_XEUS)
 UE_frame = pd.read_sql(UE_sql,conn_XEUS)
 atlas_output  = pd.DataFrame(columns= atlas_frame.columns)
 UE_output = pd.DataFrame(columns= UE_frame.columns)
-print(new_row)
+dne_output = pd.DataFrame(columns = ['ENTITY'])
 
-for j, row in new_row.iterrows():
-    if(new_row.loc[0, 'VALUE'] == 'UTP'):
+
+for j, row in ent_input.iterrows():
+    not_exist = True
+    if(ent_input.loc[j, 'VALUE'] == 'UTP'):
         for i,row in atlas_frame.iterrows():
-            if (atlas_frame.loc[i,'ENTITY'] == new_row.loc[j,'ENTITY']):
-                atlas_frame.loc[i,'EXCLUDETODATE'] = datetime.now()
+            if (atlas_frame.loc[i,'ENTITY'] == ent_input.loc[j,'ENTITY']):
+                not_exist = False
                 atlas_output =  pd.concat([atlas_output,atlas_frame.iloc[[i]]])
+                atlas_output.loc[i,'EXCLUDETODATE'] = datetime.now()
                 break
-    elif(new_row.loc[0, 'VALUE'] == 'Decon'):
+    elif(ent_input.loc[j, 'VALUE'] == 'Decon'):
         for i,row in atlas_frame.iterrows():
-            if (atlas_frame.loc[i,'ENTITY'] == new_row.loc[j,'ENTITY']):
+            if (atlas_frame.loc[i,'ENTITY'] == ent_input.loc[j,'ENTITY']):
+                not_exist = False
                 atlas_output =  pd.concat([atlas_output,atlas_frame.iloc[[i]]])
-                atlas_output.loc[j,'EXCLUDETODATE'] = new_row.loc[j,'DATE']
+                atlas_output.loc[i,'EXCLUDEFROMDATE'] = ent_input.loc[j,'DATE']
+                atlas_output.loc[i,'EXCLUDETODATE']  = '2999-12-31 12:00:00'
                 break  
-    if(new_row.loc[0, 'VALUE'] == 'UTP'):
+    if(ent_input.loc[j, 'VALUE'] == 'UTP'):
         for i,row in UE_frame.iterrows():
-            if (UE_frame.loc[i,'ENTITY'] == new_row.loc[j,'ENTITY']):
+            if (UE_frame.loc[i,'ENTITY'] == ent_input.loc[j,'ENTITY']):
+                not_exist = False
                 UE_frame.loc[i, 'INCLUDE'] = 'Y'
                 UE_output = pd.concat([UE_output, UE_frame.iloc[[i]]])
                 break
-    elif(new_row.loc[0, 'VALUE'] == 'Decon'):
+    elif(ent_input.loc[j, 'VALUE'] == 'Decon'):
         for i,row in atlas_frame.iterrows():
-            if (UE_frame.loc[i,'ENTITY'] == new_row.loc[j,'ENTITY']):
+            if (UE_frame.loc[i,'ENTITY'] == ent_input.loc[j,'ENTITY']):
+                not_exist = False
                 UE_frame.loc[i, 'INCLUDE'] = 'N'
                 UE_output = pd.concat([UE_output, UE_frame.iloc[[i]]])  
                 break  
-
-print(atlas_output)
-print(UE_output)
-atlas_output.to_csv(r"C:\Users\bhowell\Downloads\test.csv")
-
+    if(not_exist == True):
+        dne_output.loc[-1]  = ent_input.loc[j,'ENTITY']
+        dne_output.index = dne_output.index + 1
+        dne_output = dne_output.sort_index()
+empty_csv = pd.DataFrame(columns = ent_input.columns)
+atlas_output.to_csv(r"\\rf3p-nas-XEUS_Reports.rf3prod.mfg.intel.com\XEUS_Reports\AFO\P1274\General\AFO\STRS\F_UDF_ATLAS_EQP_EXCLUDE\Atlas_MRCL.csv", index = False)
+UE_output.to_csv(r"\\rf3p-nas-XEUS_Reports.rf3prod.mfg.intel.com\XEUS_Reports\AFO\P1274\General\AFO\STRS\F_UE_CONFIG\UE_MRCL.csv", index = False)
+dne_output.to_csv(r"C:\Users\bhowell\OneDrive - Intel Corporation\ME\Decon_MRCL\Tool_Exist.csv", index = False)
+empty_csv.to_csv(r"C:\Users\bhowell\OneDrive - Intel Corporation\ME\STRS upload test.csv", index = False)
